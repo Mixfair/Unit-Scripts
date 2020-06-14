66907da2468248650e79c318e71705b2fbf39294
Ujin <jenia1702@gmail.com>
2020-06-14 04:25:39 +0300

    v1.0.3

--- UIBase.cs
+++ UIBase.cs
@@ -30,7 +30,7 @@ namespace Oxide.Plugins
             {
                 foreach(var ui_inst in CUIBuild.getItems()){
                     CuiHelper.DestroyUi(player, ui_inst);
-                }
+                } 
             } 
         }
 
@@ -41,31 +41,115 @@ namespace Oxide.Plugins
   
             var peace = new CUIPeace{ 
                 panelName = "Menu",
-                horizontal = ".1 .2"
+                aMax = ".7 .6",
+                aMin = ".1 .4",
+                horizontal = ".4 .4",
+                vertical = ".4 .4"
             };
 
         }   
 
         public class CUIPeace{
+            
             public string panelName;
             public string color;
-
+            public double aMaxh;
+            public double aMinh;
+            public double aMaxv;
+            public double aMinv;
+            
+            public bool cursor = false;
+            
+            public string command;
+            public TextAnchor align = TextAnchor.MiddleCenter;
+            
             public string horizontal
             { 
                 set
                 {
-                    double x1,y1;
+                    double x1,x2,outx1,outx2;
                     string[] coord = value.Split(' ');
                     Double.TryParse(coord[0], out x1);
-                    Double.TryParse(coord[1], out y1);
-                    
-                } 
-                get { return "string"; } 
-            }
-            public string vertical{ set; get; }
-            public bool cursor = false;
+                    Double.TryParse(coord[1], out x2);
 
-            static public string ToGeneral(double x1, double x2, double y1, double y2){
+                    ToGeneral( x1, x2, aMinh, aMaxh, out outx1, out outx2);
+
+                    aMinh = outx1;
+                    aMaxh = outx2;
+                } 
+                get 
+                {
+                    string res = aMinh.ToString("G", CultureInfo.InvariantCulture) + " " + aMinv.ToString("G", CultureInfo.InvariantCulture);
+
+                    return res;
+                } 
+            }
+
+            public string vertical
+            {                 
+                set
+                {
+                    double y1,y2,outy1,outy2;
+                    string[] coord = value.Split(' ');
+                    Double.TryParse(coord[0], out y1);
+                    Double.TryParse(coord[1], out y2);
+
+                    ToGeneral( y1, y2, aMinv, aMaxv, out outy1, out outy2);
+
+                    aMinv = outy1;
+                    aMaxv = outy2;
+                } 
+                get { 
+
+                    string res = aMaxh.ToString("G", CultureInfo.InvariantCulture) + " " + aMaxv.ToString("G", CultureInfo.InvariantCulture);
+                    
+                    return res; 
+                
+                }  
+            }
+
+            public string aMax
+            { 
+                set
+                {
+                    double y1,y2;
+                    string[] coord = value.Split(' ');
+                    Double.TryParse(coord[0], out y1);
+                    Double.TryParse(coord[1], out y2);
+
+                    aMaxh = y1;
+                    aMaxv = y2;
+                } 
+                get 
+                {
+                    string res = aMaxh.ToString("G", CultureInfo.InvariantCulture) + " " + aMaxv.ToString("G", CultureInfo.InvariantCulture);
+
+                    return res;
+                } 
+            }
+
+            public string aMin
+            { 
+                set
+                {
+                    double y1,y2;
+                    string[] coord = value.Split(' ');
+                    Double.TryParse(coord[0], out y1);
+                    Double.TryParse(coord[1], out y2);
+
+                    aMinh = y1;
+                    aMinv = y2;
+                } 
+                get 
+                { 
+                    
+                    string res = aMinh.ToString("G", CultureInfo.InvariantCulture) + " " + aMinv.ToString("G", CultureInfo.InvariantCulture);
+
+                    return res;
+                } 
+            }
+
+            static public void ToGeneral(double x1, double x2, double y1, double y2, out double outx1, out double outx2){
                 string res;
                 double temp1, temp2, width;
                 
@@ -73,9 +157,8 @@ namespace Oxide.Plugins
                 temp1 = y1 + (x1*width);
                 temp2 = y2 - (x2*width);
 
-                res = temp1.ToString("G", CultureInfo.InvariantCulture) + " " + temp2.ToString("G", CultureInfo.InvariantCulture);
-                
-                return res;
+                outx1 = temp1;
+                outx2 = temp2;
             }
 
         }
@@ -119,22 +202,22 @@ namespace Oxide.Plugins
                 return Element;
             }
 
-            static public void CreatePanel(ref CuiElementContainer container, string panelName, string color, string aMin, string aMax, bool cursor = false)
+            static public void CreatePanel(CUIPeace panel, ref CuiElementContainer container)
             {   
 
                 container.Add(new CuiPanel
                 {
-                    Image = { Color = color, Material = material },
-                    RectTransform = { AnchorMin = aMin, AnchorMax = aMax },
-                    CursorEnabled = cursor 
+                    Image = { Color = panel.color, Material = material },
+                    RectTransform = { AnchorMin = panel.horizontal, AnchorMax = panel.vertical },
+                    CursorEnabled = panel.cursor 
                 },
-                panelName);
+                panel.panelName);
 
             } 
 
             static public void CreateLabel(ref CuiElementContainer container, string panel, string color, string text, int size, string aMin, string aMax, TextAnchor align = TextAnchor.MiddleCenter)
             {
-                container.Add(new CuiLabel
+                container.Add(new CuiLabel 
                 {
                     Text = {Font = font, Color = color, FontSize = size, Align = align, FadeIn = 1.0f, Text = text },
                     RectTransform = { AnchorMin = aMin, AnchorMax = aMax }
@@ -190,6 +273,7 @@ namespace Oxide.Plugins
             BasePlayer target;
             CuiElementContainer maincont;
             string ui_name;
+
             private static Dictionary<string, string> UIColors = new Dictionary<string, string>
             {
                 {"black_alpha", ".05 .05 .05 .9" },
@@ -199,6 +283,15 @@ namespace Oxide.Plugins
                 
             };
 
+            public string aMinInfo = ".151 .365";
+            public string aMaxInfo = ".36 .85";
+
+            public string aMinTp = ".151 .21";
+            public string aMaxTp = ".36 .35";
+
+            public string aMinBody = ".37 .21";
+            public string aMaxBody = ".84 .85";
+
             public UIMenu(BasePlayer player){
                 ui_name = "menu";
                 target = player;
@@ -206,10 +299,10 @@ namespace Oxide.Plugins
                 buildOverlay();
                 buildInfo();
  
-                CUIBuild.CreateLabel(ref maincont, "menu", ".8 .8 .5 1", "string text", 14, ".2 .4", ".5 .5");
-                
-                CUIBuild.CreateButton(ref maincont, "menu", ".8 .8 .5 1", "button", 14, ".45 .45", ".5 .5", "chat.say command",TextAnchor.UpperLeft);
-                CUIBuild.LoadImage(ref maincont, "menu", "http://i.imgur.com/xxQnE1R.png", ".45 .45", ".65 .65");
+                //CUIBuild.CreateLabel(ref maincont, "menu", ".8 .8 .5 1", "string text", 14, ".2 .4", ".5 .5");
+                //
+                //CUIBuild.CreateButton(ref maincont, "menu", ".8 .8 .5 1", "button", 14, ".45 .45", ".5 .5", "chat.say command",TextAnchor.UpperLeft);
+                //CUIBuild.LoadImage(ref maincont, "menu", "http://i.imgur.com/xxQnE1R.png", ".45 .45", ".65 .65");
     
                 CuiHelper.AddUi(target, maincont);   
     
@@ -218,24 +311,51 @@ namespace Oxide.Plugins
             } 
 
             public void buildOverlay(){
-                
+
                 CUIBuild.setMaterial("assets/content/ui/uibackgroundblur-ingamemenu.mat");
                 maincont = CUIBuild.CreateContainer("menu", ".1 .1 .1 .5", "0 0", "1 1", true);
                 CUIBuild.setMaterial("default"); 
-                CUIBuild.CreatePanel(ref maincont, ui_name, UIColors["black_alpha"], ".151 .365", ".36 .85");
-                CUIBuild.CreatePanel(ref maincont, ui_name, UIColors["black_alpha"], ".151 .21", ".36 .35");
-                CUIBuild.CreatePanel(ref maincont, ui_name, UIColors["black_alpha"], ".37 .21", ".84 .85");
+
+                var panel = new CUIPeace{
+                    panelName = ui_name, 
+                    color = UIColors["black_alpha"],
+                    aMax = aMaxInfo,
+                    aMin = aMinInfo
+                };
+
+                CUIBuild.CreatePanel(panel, ref maincont);
+                
+                panel.aMin = aMinTp;
+                panel.aMax = aMaxTp;
+
+                CUIBuild.CreatePanel(panel, ref maincont);
+
+                panel.aMin = aMinBody;
+                panel.aMax = aMaxBody; 
+
+                CUIBuild.CreatePanel(panel, ref maincont);
  
             }
 
-            public void buildInfo(){
-                
+            public void buildInfo(){ 
 
-                CUIBuild.CreatePanel(ref maincont, ui_name,  UIColors["black"], ".151 .81", ".36 .85");
-                CUIBuild.CreatePanel(ref maincont, ui_name,  UIColors["black"], ".151 .365", ".36 .41");
-                CUIBuild.CreatePanel(ref maincont, ui_name,  UIColors["orange"], ".151 .41", ".36 .5");
-                CUIBuild.LoadImage(ref maincont, ui_name, "http://www.rigormortis.be/wp-content/uploads/rust-icon-512.png", ".194 .53", ".33 .78");
-                CUIBuild.CreateLabel(ref maincont, ui_name, UIColors["white_pink"], target.ToString(), 18,  ".153 .47", ".36 .5");
+                var panel = new CUIPeace{ 
+                    panelName = ui_name,
+                    color = UIColors["black"],
+                    aMax = ".7 .7",
+                    aMin = ".1 .1",
+                    horizontal = ".1 .7",
+                    vertical = ".1 .7"
+                };  
+
+                Debug.Log(panel.aMax);
+                Debug.Log(panel.aMin);
+
+                CUIBuild.CreatePanel(panel, ref maincont);
+                //CUIBuild.CreatePanel(ref maincont, ui_name,  UIColors["black"], ".151 .365", ".36 .41");
+                //CUIBuild.CreatePanel(ref maincont, ui_name,  UIColors["orange"], ".151 .41", ".36 .5");
+                //CUIBuild.LoadImage(ref maincont, ui_name, "http://www.rigormortis.be/wp-content/uploads/rust-icon-512.png", ".194 .53", ".33 .78");
+                //CUIBuild.CreateLabel(ref maincont, ui_name, UIColors["white_pink"], target.ToString(), 18,  ".153 .47", ".36 .5");
 
 
             }
